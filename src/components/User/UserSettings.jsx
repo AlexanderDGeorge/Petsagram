cc68aa3317e3ab5e6c67c2bc29400ac04e1bc7cd
Alexander George <alexander97.george@gmail.com>
2020-06-04 13:21:19 -0700

    add input fields for edit profile

--- src/components/User/UserSettings.jsx
+++ src/components/User/UserSettings.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useLocation, useHistory, Switch, Route } from "react-router-dom";
+import { UserContext } from "../Application";
 
 export default function UserSettings() {
   return (
@@ -14,7 +15,59 @@ export default function UserSettings() {
 }
 
 function EditProfile() {
-  return <section id="EditProfile" className="USContent"></section>;
+  const { user, setUser } = useContext(UserContext);
+  const [name, setName] = useState(user.name);
+  const [username, setUsername] = useState(user.username);
+  const [bio, setBio] = useState(user.bio);
+
+  return (
+    <section id="EditProfile" className="USContent">
+      <div className="editarea">
+        <img
+          className="label"
+          src={user.photoURL}
+          alt=""
+          style={{ borderRadius: "50%" }}
+        />
+        <div className="input">
+          <label htmlFor="file-upload">Change Profile Picture</label>
+          <input
+            id="file-upload"
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+          />
+        </div>
+      </div>
+      <div className="editarea">
+        <h3 className="label">Name</h3>
+        <input
+          className="input"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="editarea">
+        <h3 className="label">Username</h3>
+        <input
+          className="input"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="editarea">
+        <h3 className="label">Bio</h3>
+        <input
+          className="input"
+          type="text"
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+        />
+      </div>
+    </section>
+  );
 }
 
 function ChangePassword() {
